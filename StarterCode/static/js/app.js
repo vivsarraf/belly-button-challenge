2b58056bbd92b46a4b2b605c8c1fd93a4f26558e
vivek sarraf <sarraf.vivek@gmail.com>
2024-04-09 18:35:12 -0400

    updated the dashboard logic for bar and bubble chart

---
+++ StarterCode/static/js/app.js
@@ -0,0 +1,247 @@
+// Assign the url to a constant variable
+const url = 'https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json';
+
+// Use D3 library to read the JSON file from the URL
+d3.json(url).then(function(data){
+    console.log(data);
+}); 
+
+// Initialize the dashboard  
+function init() {
+
+    // Use D3 to select the dropdown menu
+    let dropdownMenu = d3.select("#selDataset");
+
+    // Use D3 to get sample names and populate the drop-down selector
+    d3.json(url).then((data) => {
+        
+        // Set a variable for the sample names
+        let names = data.names;
+
+        // Add  samples to dropdown menu
+        names.forEach((id) => {
+
+            // print the value of the variables during the entire loop
+            console.log(id);
+
+            dropdownMenu.append("option")
+            .text(id)
+            .property("value",id);
+        });
+
+        // Set the first sample from the list
+        let sample_one = names[0];
+
+        // Print the value of sample_one
+        console.log(sample_one);
+
+        // Build the initial plots
+        buildMetadata(sample_one);
+        buildBarChart(sample_one);
+        buildBubbleChart(sample_one);
+        buildGaugeChart(sample_one);
+        //buildPieChart(sample_one);
+    });
+};
+
+// Function to populate metadata info
+function buildMetadata(sample) {
+
+    // Use D3 to retrieve all data
+    d3.json(url).then((data) => {
+
+        // Retrieve all metadata
+        let metadata = data.metadata;
+
+        // Filter based on the value of the sample
+        let value = metadata.filter(result => result.id == sample);
+
+        // Print the array of metadata objects
+        console.log(value)
+
+        // Get the first index from the array
+        let valueData = value[0];
+
+        // Clear metadata content to make it ready for user input
+        d3.select("#sample-metadata").html("");
+
+        // Use Object.entries to add each key/value pair to the panel
+        Object.entries(valueData).forEach(([key,value]) => {
+
+            // Print the individual key/value pairs as they are being appended to the metadata panel
+            console.log(key,value);
+
+            d3.select("#sample-metadata").append("h5").text(`${key}: ${value}`);
+        });
+    });
+
+};
+
+// Function to populate the bar chart
+function buildBarChart(sample) {
+
+    // Use D3 to retrieve all of the data
+    d3.json(url).then((data) => {
+
+        // Retrieve all sample data
+        let sampleInfo = data.samples;
+
+        //Find the selected sample
+        //let selectedSample = sampleInfo.find(result => r //
+
+        // Filter based on the value of the sample
+        let value = sampleInfo.filter(result => result.id == sample);
+
+        // Get the first index from the array
+        let valueData = value[0];
+
+        // Get the otu_ids, lables, and sample values
+        let otu_ids = valueData.otu_ids;
+        let otu_labels = valueData.otu_labels;
+        let sample_values = valueData.sample_values;
+
+        // Log the data to the console
+        console.log(otu_ids,otu_labels,sample_values);
+
+        // Set top ten items to display in descending order
+        let yticks = otu_ids.slice(0,10).map(id => `OTU ${id}`).reverse();
+        let xticks = sample_values.slice(0,10).reverse();
+        let labels = otu_labels.slice(0,10).reverse();
+        
+        // Set up the trace for the bar chart
+        let trace = {
+            x: xticks,
+            y: yticks,
+            text: labels,
+            type: 'bar',
+            orientation: 'h',
+            //marker:{
+                //color:'blue'
+            //}   
+        };
+
+        // Setup the layout
+        let layout = {
+            title: "Top 10 OTUs Present"
+        };
+
+        // Call Plotly to plot the bar chart
+        Plotly.newPlot("bar", [trace], layout)
+    });
+};
+
+// Function to populate the bubble chart
+function buildBubbleChart(sample) {
+
+    // Use D3 to retrieve all of the data
+    d3.json(url).then((data) => {
+        
+        // Retrieve all sample data
+        let sampleInfo = data.samples;
+
+        // Filter based on the value of the sample
+        let value = sampleInfo.filter(result => result.id == sample);
+
+        // Get the first index from the array
+        let valueData = value[0];
+
+        // Get the otu_ids, lables, and sample values
+        let otu_ids = valueData.otu_ids;
+        let otu_labels = valueData.otu_labels;
+        let sample_values = valueData.sample_values;
+
+        // Log the data to the console
+        console.log(otu_ids,otu_labels,sample_values);
+        
+        // Set up the trace for bubble chart
+        let trace1 = {
+            x: otu_ids,
+            y: sample_values,
+            text: otu_labels,
+            mode: "markers",
+            marker: {
+                size: sample_values,
+                color: otu_ids,
+                colorscale: "Earth"
+            }
+        };
+
+        // Set up the layout
+        let layout = {
+            title: "Bacteria Per Sample",
+            hovermode: "closest",
+            xaxis: {title: "OTU ID"},
+        };
+
+        // Call Plotly to plot the bubble chart
+        Plotly.newPlot("bubble", [trace1], layout)
+    });
+};
+
+//Function to populate the Gauge Chart
+function buildGaugeChart(sample) {
+    // Use D3 to retrieve all of the data
+    d3.json(url).then((data) => {
+      
+      //Assiging metadata to a variable
+      let metadata = data.metadata;
+      
+      // Filter based on values of the sample
+      let resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+      
+      // Get the first index from the array
+      let result = resultArray[0];
+      
+      // Get the washing frequency value
+      let wfreq = result.wfreq;
+      
+      // Set up the trace for the gauge chart
+      let trace = [
+        {
+          domain: { x: [0, 1], y: [0, 1] },
+          value: wfreq,
+          title: { text: "Belly Button Washing Frequency" },
+          type: "indicator",
+          mode: "gauge+number",
+          gauge: {
+            axis: { range: [null, 9] },
+            bar: { color: "darkblue" },
+            steps: [
+              { range: [0, 1], color: "#f7fcfd" },
+              { range: [1, 2], color: "#e5f5f9" },
+              { range: [2, 3], color: "#ccece6" },
+              { range: [3, 4], color: "#99d8c9" },
+              { range: [4, 5], color: "#66c2a4" },
+              { range: [5, 6], color: "#41ae76" },
+              { range: [6, 7], color: "#238b45" },
+              { range: [7, 8], color: "#006d2c" },
+              { range: [8, 9], color: "#00441b" }
+            ],
+          }
+        }
+      ];
+      
+      // Set up the layout
+      let layout = { width: 500, height: 400, margin: { t: 0, b: 0 } };
+      
+      // Call Plotly to plot the gauge chart
+      Plotly.newPlot("gauge", trace, layout);
+    });
+  }
+  
+// Function that updates dashboard when sample is changed
+function optionChanged(value) { 
+
+    // Log the new value
+    console.log(value); 
+
+    // Call all functions 
+    buildMetadata(value);
+    buildBarChart(value);
+    buildBubbleChart(value);
+    buildGaugeChart(value);
+    //buildPieChart(value);
+};
+
+// Initialize function
+init();
